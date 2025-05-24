76b201325e533f28217fdf0ed07aa78c67ccfa07
cantoch <cantoch@icloud.com>
2025-05-25 00:00:26 +0200

    Exercice terminé

--- UserList/Sources/ViewModel/UserListViewModel.swift
+++ UserList/Sources/ViewModel/UserListViewModel.swift
@@ -12,42 +12,37 @@ class UserListViewModel: ObservableObject {
     @Published var users: [User] = []
     @Published var isLoading: Bool = false
     @Published var isGridView = false
-    
+
     private let repository: UserListRepository
+
     init(repository: UserListRepository) {
         self.repository = repository
     }
-    
+
     // MARK: - Outputs
-    
+
     func shouldLoadMoreData(currentItem item: User) -> Bool {
         guard let lastItem = users.last else { return false }
         return !isLoading && item.id == lastItem.id
     }
-    
+
     // MARK: - Inputs
+
     @MainActor
     func fetchUsers() async {
         isLoading = true
         do {
             let users = try await repository.fetchUsers(quantity: 20)
             self.users.append(contentsOf: users)
-            
         } catch {
             print("Error fetching users: \(error.localizedDescription)")
         }
         isLoading = false
     }
-    
+
     @MainActor
     func reloadUsers() async {
         users.removeAll()
         await fetchUsers()
     }
 }
-
-
-
-
-
-
